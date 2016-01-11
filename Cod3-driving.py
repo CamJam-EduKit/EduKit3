2081b21a06118e675f84bd59cd6fb1417f6c960b
CamJam-EduKit <tim@potton.me.uk>
2016-01-11 20:18:36 +0000

    Create Cod3-driving.py

---
+++ Cod3-driving.py
@@ -0,0 +1,44 @@
+# CamJam EduKit 3 - Robotics
+# Worksheet 3 – Driving and Turning
+
+import RPi.GPIO as GPIO # Import the GPIO Library
+import time # Import the Time library
+
+# Set the GPIO modes
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Set variables for the GPIO motor pins
+pinMotorAForwards = 10
+pinMotorABackwards = 9
+pinMotorBForwards = 8
+pinMotorBBackwards = 7
+
+# Set the GPIO Pin mode
+GPIO.setup(pinMotorAForwards, GPIO.OUT)
+GPIO.setup(pinMotorABackwards, GPIO.OUT)
+GPIO.setup(pinMotorBForwards, GPIO.OUT)
+GPIO.setup(pinMotorBBackwards, GPIO.OUT)
+
+# Turn all motors off
+def StopMotors():
+    GPIO.output(pinMotorAForwards, 0)
+    GPIO.output(pinMotorABackwards, 0)
+    GPIO.output(pinMotorBForwards, 0)
+    GPIO.output(pinMotorBBackwards, 0)
+
+# Turn both motors forwards
+def Forwards():
+    GPIO.output(pinMotorAForwards, 1)
+    GPIO.output(pinMotorABackwards, 0)
+    GPIO.output(pinMotorBForwards, 1)
+    GPIO.output(pinMotorBBackwards, 0)
+
+# Turn both motors backwards
+def Backwards():
+    GPIO.output(pinMotorAForwards, 0)
+    GPIO.output(pinMotorABackwards, 1)
+    GPIO.output(pinMotorBForwards, 0)
+    GPIO.output(pinMotorBBackwards, 1)
+
+GPIO.cleanup()
