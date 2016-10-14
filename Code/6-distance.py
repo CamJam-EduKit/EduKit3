d7117c4f924818340f9ffba4a36ac7f125c230aa
Neil Bizzell <NBizzell@users.noreply.github.com>
2016-10-14 21:22:19 +0100

    Update 6-distance.py
    Change for GPIO Zero Library

--- Code/6-distance.py
+++ Code/6-distance.py
@@ -1,68 +1,19 @@
 # CamJam EduKit 3 - Robotics
-# Worksheet 6 – Measuring Distance
+# Worksheet 6 – Measuring Distance GPIO Zero 
 
-import RPi.GPIO as GPIO # Import the GPIO Library
-import time # Import the Time library
-
-# Set the GPIO modes
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+from gpiozero import DistanceSensor # Import the GPIOZero Library
+from time import sleep # Import the cleep frim the time Library
 
 # Define GPIO pins to use on the Pi
-pinTrigger = 17
-pinEcho = 18
+sensor = DistanceSensor(echo=18, trigger=17)
 
 print("Ultrasonic Measurement")
 
-# Set pins as output and input
-GPIO.setup(pinTrigger, GPIO.OUT)  # Trigger
-GPIO.setup(pinEcho, GPIO.IN)      # Echo
-
-try:
-    # Repeat the next indented block forever
-    while True:
-        # Set trigger to False (Low)
-        GPIO.output(pinTrigger, False)
-
-        # Allow module to settle
-        time.sleep(0.5)
-
-        # Send 10us pulse to trigger
-        GPIO.output(pinTrigger, True)
-        time.sleep(0.00001)
-        GPIO.output(pinTrigger, False)
-
-        # Start the timer
-        StartTime = time.time()
-
-        # The start time is reset until the Echo pin is taken high (==1)
-        while GPIO.input(pinEcho)==0:
-            StartTime = time.time()
-
-        # Stop when the Echo pin is no longer high - the end time
-        while GPIO.input(pinEcho)==1:
-            StopTime = time.time()
-            # If the sensor is too close to an object, the Pi cannot
-            # see the echo quickly enough, so we have to detect that
-            # problem and say what has happened.
-            if StopTime-StartTime >= 0.04:
-                print("Hold on there!  You're too close for me to see.")
-                StopTime = StartTime
-                break
-
-        # Calculate pulse length
-        ElapsedTime = StopTime - StartTime
-
-        # Distance pulse travelled in that time is
-        # time multiplied by the speed of sound (cm/s)
-        Distance = ElapsedTime * 34326
-
-        # That was the distance there and back so halve the value
-        Distance = Distance / 2
-
-        print("Distance: %.1f cm" % Distance)
-
-        time.sleep(0.5)
+# Repeat the next indented block forever
+while True:
+    #print the distance in meters
+    print('Distance: ', sensor.distance * 100) #check this lin GPIO Zero documentattion says distance gives distance in meters but the multiplies by 100 here to get meters?
+    sleep(0.5)
 
 # If you press CTRL+C, cleanup and stop
 except KeyboardInterrupt:
