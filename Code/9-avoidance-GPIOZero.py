4030a2bfaf4b5a1cc3c1ed36d331cd177a5f7fe0
Neil Bizzell <NBizzell@users.noreply.github.com>
2016-10-18 21:45:49 +0100

    Update 9-avoidance-GPIOZero.py
    Comments edited non functional code removed.

--- Code/9-avoidance-GPIOZero.py
+++ Code/9-avoidance-GPIOZero.py
@@ -2,27 +2,23 @@
 # Worksheet 9 – Obstacle Avoidance
 
 from gpiozero import DistanceSensor, CamJamKitRobot # Import DistanceSensor and CamJamKitRobot from the GPIOzero Library
-from time import sleep # Import sleep from the Time library
+from time import sleep # Import sleep from the time library
 
-# define the pins for the motor (fixed in the GPIO Zero Lobrary)
+# define the pins for the motor (fixed in the GPIO Zero Library)
 robot = CamJamKitRobot()
 
-# Define GPIO pins to use fir the ultrasonic sensor
+# Define GPIO pins to use for the ultrasonic sensor
 sensor = DistanceSensor(echo=18, trigger=17)
 
 #define how to avoid the obstacle (simple version, just turn away)
 def AvoidObstacle():
-    robot.left()
+    robot.left() #turn left
     sleep(0.5)
     
 #repeat the next indented block forever
 while True:
-    robot.forward()
+    robot.forward()#drive forward
         sleep(0.1)
-        if sensor.distance < 0.3: #check units documentaiton is confusing this should be 0.3m but could be o.3cm) could use if sensor.when_in_range:?
-            robot.stop()
-            AvoidObstacle()
-
-# If you press CTRL+C, cleanup and stop
-except KeyboardInterrupt:
-    GPIO.cleanup()
+        if sensor.distance < 0.3: #check if sensor distance is less than 0.3m
+            robot.stop() #stop the robot
+            AvoidObstacle() #call the AvoidObstacle function
