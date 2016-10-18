9c6a2b520618d12b4f2ceb6bfde0608deaf751bd
Neil Bizzell <NBizzell@users.noreply.github.com>
2016-10-18 21:47:27 +0100

    Update 5-line-GPIOZero.py
    Comments edited non functional code removed

--- Code/5-line-GPIOZero.py
+++ Code/5-line-GPIOZero.py
@@ -1,8 +1,8 @@
 # CamJam EduKit 3 - Robotics
 # Worksheet 5 – Line Detection
 
-from gpiozero import LineSensor # Import the LineSensor from the GPIOZero Library
-from signal import pause # import pause frm the signal library
+from gpiozero import LineSensor # Import LineSensor from the GPIOZero Library
+from signal import pause # import pause from the signal library
 
 #set up the GPIO pin for the line sensor
 sensor = LineSensor(25)
@@ -11,7 +11,3 @@ sensor = LineSensor(25)
 sensor.when_line = lambda: print('The sensor is seeing a black surface')
 sensor.when_no_line = lambda: print('The sensor is seeing a white surface')
 pause()
-
-# If you press CTRL+C, cleanup and stop
-except KeyboardInterrupt:
-    GPIO.cleanup()
