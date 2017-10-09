673dfd1e893efe7befa0dd7df987d4172561ec65
Neil Bizzell <NBizzell@users.noreply.github.com>
2017-10-09 21:44:16 +0100

    Update linefollower.py

--- GPIOZero/Code/linefollower.py
+++ GPIOZero/Code/linefollower.py
@@ -1,9 +1,16 @@
-from gpiozero import LineSensor, CamJamKitRobot
-from signal import pause
+# CamJam EduKit 3 - Robotics
+# Line Follower GPIO Zero version (untested)
+# by Neil Bizzell (@PiVangelist)
 
+from gpiozero import LineSensor, CamJamKitRobot # import LineSensor and CamJamKit Robot objects from GPIO Zero library
+from signal import pause # import pause from signal library
+
+#define sensor as instance of LineSensor Class and define pin as Pin 25
 sensor = LineSensor(25)
+#Define robot as instance of CamJamKit Robot Class
 robot = CamJamKitRobot()
 
+#set robot values when line detected 1 is motor on 0 is motor off
 sensor.when_line = lambda: robot.value = (1, 0)
 sensor.when_no_line = lambda: robot.value = (0, 1)
 pause()
