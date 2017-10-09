70e008909581eb265514c4829b3e0da1596bb7bc
Neil Bizzell <NBizzell@users.noreply.github.com>
2017-10-09 12:02:57 +0100

    Update 5-line-GPIOZero.py

--- Code/5-line-GPIOZero.py
+++ Code/5-line-GPIOZero.py
@@ -8,7 +8,7 @@ from signal import pause # import pause from the signal library
 #set up the GPIO pin for the line sensor
 sensor = LineSensor(25)
 
-
+#print messages for white or black surface
 sensor.when_line = lambda: print('The sensor is seeing a black surface')
 sensor.when_no_line = lambda: print('The sensor is seeing a white surface')
 pause()
