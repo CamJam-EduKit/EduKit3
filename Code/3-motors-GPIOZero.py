29e163ee019bb3035560bdd69dfb057a8a262f6a
Neil Bizzell <NBizzell@users.noreply.github.com>
2017-10-09 12:00:57 +0100

    Update and rename 3-motors.py to 3-motors-GPIOZero.py
    Creating GPIO Zero version of motor test

---
+++ Code/3-motors-GPIOZero.py
@@ -0,0 +1,9 @@
+# CamJam EduKit 3 - Robotics
+# Worksheet 3 - Motor Test Code GPIO Zero Version
+# by Neil Bizzell (@PiVangelist)
+
+from gpiozero import CamJamKitRobot
+from time import sleep 
+
+robot.forward(1)
+sleep(2)
